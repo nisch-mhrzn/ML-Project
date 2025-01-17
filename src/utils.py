228f4152d00f27ee0744db64e0b8d73ddcb0fe42
Nischal <nischal.maharjan1233@gmail.com>
2025-01-17 11:49:49 +0545

    `Add Flask web application for student exam performance prediction`

--- src/utils.py
+++ src/utils.py
@@ -22,45 +22,41 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for model_name, model in models.items():
-            try:
-                # Ensure parameters exist for the current model
-                para = param.get(model_name, {})
-                gs = GridSearchCV(model, para, cv=3, n_jobs=-1)
-                gs.fit(X_train, y_train)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-                # Set the best parameters and retrain
-                model.set_params(**gs.best_params_)
-                model.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-                # Predictions
-                y_train_pred = model.predict(X_train)
-                y_test_pred = model.predict(X_test)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-                # R2 Scores
-                train_model_score = r2_score(y_train, y_train_pred)
-                test_model_score = r2_score(y_test, y_test_pred)
+            #model.fit(X_train, y_train)  # Train model
 
-                # Save test score in report
-                report[model_name] = test_model_score
+            y_train_pred = model.predict(X_train)
 
-            except Exception as model_error:
-                print(f"Error with model {model_name}: {model_error}")
-                continue
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
-
+    
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
